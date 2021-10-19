62deed9cf862fbd53459a0b46d31d9f556b17478
DESKTOP-EJDVHOT\parkj <park.james1030@gmail.com>
2021-10-19 12:54:19 -0500

    setup  and components.

---
+++ src/App.js
@@ -0,0 +1,6 @@
+import React from "react";
+import youtube from './api/youtube';
+import {SearchBar, VideoList, VideoDetail} from './components';
+
+
+
---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+class SearchBar extends React.Component{
+    state={
+        searchVid:'',
+    }
+    render(){ 
+        return (
+        <h1>searchbar comp</h1>
+        )
+    }
+}
